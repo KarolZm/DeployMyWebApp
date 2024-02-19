a8a76b11fb8778d450c8b713e4fa1a93f8877381
Karol <ka.zmorski@gmail.com>
2024-02-19 18:48:06 +0100

    Add procfile for gunicorn

--- main.py
+++ main.py
@@ -10,11 +10,14 @@ from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Text, ForeignKey
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
-# Import your forms from the forms.py
+from dotenv import load_dotenv
+# Import forms from the forms.py
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
+load_dotenv()
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.urandom(12)
+app.config['SECRET_KEY'] = os.getenv('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -39,7 +42,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URI", 'sqlite:///blog.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -266,4 +269,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False)
